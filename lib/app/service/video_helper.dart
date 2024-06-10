11a7e7689640ab01dc3e0cafec48a51cba3153d9
Umair Hashmi <umairhashmibitsclan@gmail.com>
2024-06-10 18:01:40 +0500

    ui

--- lib/app/service/video_helper.dart
+++ lib/app/service/video_helper.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
